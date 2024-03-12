8a68c412d7feaeff7178827f2c8dbabdbba6d465
Deodate <ajadeo04@gmail.com>
2024-03-13 00:16:22 +0200

    Committing changes before updating Angular dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MediaChange, MediaObserver } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'jiraworkflow';
+  deviceXs: boolean | undefined;
+  mediaSub: Subscription | undefined;
+
+  constructor(public mediaObserver: MediaObserver) {}
+
+  ngOnInit() {
+    this.mediaSub = this.mediaObserver.asObservable().subscribe((changes: MediaChange[]) => {
+      changes.forEach(change => {
+        console.log(change.mqAlias);
+        this.deviceXs = change.mqAlias === 'xs' ? true : false;
+      });
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.mediaSub) {
+      this.mediaSub.unsubscribe();
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatIconModule } from '@angular/material/icon'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexLayoutModule} from '@angular/flex-layout';
+import { JiraComponent } from './components/jira/jira.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JiraComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    MatIconModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
