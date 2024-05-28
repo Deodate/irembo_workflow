396f84207f4b85e87383aad96d219a28633aa789
Deodate <ajadeo04@gmail.com>
2024-05-28 13:26:19 +0200

    Angular Form Design TypeScript Progress

--- src/app/components/my-textarea/my-textarea.component.ts
+++ src/app/components/my-textarea/my-textarea.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-my-textarea',
@@ -6,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-textarea.component.css']
 })
 export class MyTextareaComponent implements OnInit {
+
+  devForm: FormGroup = new FormGroup({
+    devList: new FormArray([])
+  });
+
+  getDevFields(): FormGroup{
+    return new FormGroup({
+     dev_name: new FormControl(''),
+     dev_office: new FormControl(''),
+     dev_salary: new FormControl(''),
+    });
+  }
+
+  devListArray(){
+    return this.devForm.get('DeveloperList') as FormArray;
+  }
+
+  addDev(i: number){
+    this.devListArray().push(this.getDevFields());
+  }
+
+  removeDev(i: number){
+    this.devListArray().removeAt(i);
+  }
+
+  getFormData(){
+    console.log(this.devForm.value);
+  }
 
   constructor() { }
 
